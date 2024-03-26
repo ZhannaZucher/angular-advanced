e55e79e222463bb78992690a81072578b080109b
ZhannaZucher <jzucher@gmail.com>
2024-03-26 11:25:17 +0100

    create: posts service

---
+++ src/app/social-media/services/posts.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../models/post.model';
+import { environment } from '../../../environments/environment.development';
+
+@Injectable() //comme on charge le module social-media en lazy-loading, on ne crée pas le service avec providedIn: root, sinon il sera chargé au moment du chargement du app module, mais on le déclare(le service) dans le tableau de providers du module social-media
+export class PostsService {
+  constructor(private http: HttpClient) {}
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(`${environment.apiUrl}/posts`);
+  }
+}
--- src/app/social-media/social-media.module.ts
+++ src/app/social-media/social-media.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SocialMediaRoutingModule } from './social-media-routing.module';
-
+import { PostsService } from './services/posts.service';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    SocialMediaRoutingModule
-  ]
+  imports: [CommonModule, SocialMediaRoutingModule],
+  providers: [PostsService], //comme on charge le module social-media en lazy-loading, on ne crée pas le service avec providedIn: root, sinon il sera chargé au moment du chargement du app module, mais on le déclare(le service) dans le tableau de providers du module social-media
 })
-export class SocialMediaModule { }
+export class SocialMediaModule {}
