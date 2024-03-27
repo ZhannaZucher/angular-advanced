8282aa9d50373a68320b896c9c31964108582ece
ZhannaZucher <jzucher@gmail.com>
2024-03-27 09:21:33 +0100

    create : post card

--- src/app/social-media/social-media.module.ts
+++ src/app/social-media/social-media.module.ts
@@ -6,13 +6,11 @@ import { PostsService } from './services/posts.service';
 import { PostsResolver } from './resolvers/posts.resolver';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { PostListItemComponent } from './components/post-list-item/post-list-item.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    PostListComponent,
-    PostListItemComponent
-  ],
-  imports: [CommonModule, SocialMediaRoutingModule],
+  declarations: [PostListComponent, PostListItemComponent],
+  imports: [CommonModule, SocialMediaRoutingModule, SharedModule],
   providers: [PostsService], //comme on charge le module social-media en lazy-loading, on ne crée pas le service avec providedIn: root, sinon il sera chargé au moment du chargement du app module, mais on le déclare(le service) dans le tableau de providers du module social-media
 })
 export class SocialMediaModule {}
