e077407117997ef4f82c32400fed46f8b0ae4c65
ZhannaZucher <jzucher@gmail.com>
2024-03-29 12:22:37 +0100

    update : shared module with Mat & Forms

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
+import { CommentsComponent } from './components/comments/comments.component';
+import { MaterialModule } from './material.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
-  exports: [MatToolbarModule, MatCardModule],
+  declarations: [CommentsComponent],
+  imports: [CommonModule, MaterialModule, ReactiveFormsModule],
+  exports: [CommentsComponent, MaterialModule, ReactiveFormsModule],
 })
 export class SharedModule {}
