6c1fb553f5c7be22e6bdedeaa73e6282462d1f60
ZhannaZucher <jzucher@gmail.com>
2024-03-29 12:50:48 +0100

    feat : shorten  custom pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { CommentsComponent } from './components/comments/comments.component';
 import { MaterialModule } from './material.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ShortenPipe } from './pipes/shorten.pipe';
 
 @NgModule({
-  declarations: [CommentsComponent],
+  declarations: [CommentsComponent, ShortenPipe],
   imports: [CommonModule, MaterialModule, ReactiveFormsModule],
-  exports: [CommentsComponent, MaterialModule, ReactiveFormsModule],
+  exports: [
+    CommentsComponent,
+    MaterialModule,
+    ReactiveFormsModule,
+    ShortenPipe,
+  ],
 })
 export class SharedModule {}
