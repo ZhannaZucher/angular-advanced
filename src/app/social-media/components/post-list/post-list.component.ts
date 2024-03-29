e17d7701657f6728d3c6883bf1c342878b3e1f2d
ZhannaZucher <jzucher@gmail.com>
2024-03-29 12:26:03 +0100

    feat: bind new comment event & transfert to service

--- src/app/social-media/components/post-list/post-list.component.ts
+++ src/app/social-media/components/post-list/post-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Post } from '../../models/post.model';
 import { Observable, map } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { PostsService } from '../../services/posts.service';
 
 @Component({
   selector: 'app-post-list',
@@ -10,8 +11,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PostListComponent implements OnInit {
   posts$!: Observable<Post[]>;
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService
+  ) {}
   ngOnInit(): void {
     this.posts$ = this.route.data.pipe(map((data) => data['posts']));
+  }
+
+  onPostCommented(postCommented: { comment: string; postId: number }) {
+    this.postsService.addNewComment(postCommented);
   }
 }
