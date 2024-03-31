4d0caa6aa114d90a31484a2e2bfadb6125f1d394
ZhannaZucher <jzucher@gmail.com>
2024-03-31 11:44:30 +0200

    feat: animations on comments' list

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,11 +1,40 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Comment } from '../../../core/models/comment.model';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
   styleUrl: './comments.component.scss',
+  animations: [
+    trigger('listItem', [
+      state(
+        'default',
+        style({
+          transform: 'scale(1)',
+          backgroundColor: 'white',
+          zIndex: 1,
+        })
+      ),
+      state(
+        'active',
+        style({
+          transform: 'scale(1.05)',
+          'background-color': 'rgb(201, 157, 242)',
+          'z-index': 2,
+        })
+      ),
+      transition('default => active', [animate('100ms ease-in-out')]),
+      transition('active => default', [animate('500ms ease-in-out')]),
+    ]),
+  ],
 })
 export class CommentsComponent implements OnInit {
   //Input récupère info depuis parent
@@ -13,6 +42,8 @@ export class CommentsComponent implements OnInit {
   @Input() comments!: Comment[];
   @Output() newComment = new EventEmitter<string>(); //on initialise directement avec un objet qui émet des events
   commentCtrl!: FormControl;
+  //on crée une var avec 2 types d'état possible et on lui attribue la valeur par défaut
+  listItemAnimationState: 'default' | 'active' = 'default';
 
   constructor(private formBuilder: FormBuilder) {}
 
@@ -31,5 +62,13 @@ export class CommentsComponent implements OnInit {
       this.newComment.emit(this.commentCtrl.value);
       this.commentCtrl.reset();
     }
+  }
+
+  //animations handling methods
+  onListItemMouseEnter() {
+    this.listItemAnimationState = 'active';
+  }
+  onListItemMouseLeave() {
+    this.listItemAnimationState = 'default';
   }
 }
